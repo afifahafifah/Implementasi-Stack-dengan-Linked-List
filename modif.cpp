15808797fa7645aa26545755e71f6fe82fa87b95
Gema Annisa <48683865+gemaannisa@users.noreply.github.com>
2019-04-14 20:03:50 +0700

    Update modif.cpp

--- modif.cpp
+++ modif.cpp
@@ -1,79 +1,123 @@
 #include <iostream>
 #define MAX_STACK_SIZE 10
+using namespace std;
 
-struct listnode
-{
-int data;
-listnode *next;
-};
-
-listnode *head=NULL;
-
-struct Element {
+struct Element{
+	int top;
     int data;
+    float stack[MAX_STACK_SIZE];
+    Element *next;
 };
 
 class MyStack {
     private:
-    Element stack[MAX_STACK_SIZE];
-
-    int top=-1;
+    Element stack;
+    Element *head=NULL;
+	
 
     public:
+   	void puncak(){
+   		stack.top=-1;
+	   }
+	   
     bool isEmpty(){
-        return (top<0); 
-    }
+        if(stack.top=-1){
+        	return 1;}
+        	else return 0;
+		}
+		
     bool isFull(){
-        return top == MAX_STACK_SIZE-1;
-    }
-    void push(int data){
-    	listnode *baru=new listnode;
-        Element item;
-        baru->data=data;
+        if(stack.top==MAX_STACK_SIZE-1){
+        	return 1;}
+        	else return 0;
+		}
+		
+    void push(int databaru){
+    	if(!isFull()){
+    	Element item;
+    	item.data=databaru;
+		Element *baru=new Element;
+    	baru->data=databaru;
         baru->next=head;
         head=baru;
-        if (!isFull()){
-            stack[++top]=item;
+            cout<<"Data Masuk\n"<<endl;
         } else {
-            std::cout<<"Stack Penuh"<<std::endl;
+            cout<<"Stack Penuh"<<endl;
         }
     }
-    Element pop(){
-        Element item;
-        if (!isEmpty()){
-            item = stack[top--];
+    void pop(){
+    	Element item;
+    	if(!isEmpty()){
+    		Element *baru=new Element;
+    		baru=head;
+			head=head->next;
+			delete baru;
+			cout<<"Data dipop\n";
+            stack.top--;
         } else {
-            std::cout<<"Stack Kosong"<<std::endl;
+            cout<<"Stack Kosong"<<endl;
         }
-        return item;
     }
     void printStackList(){
-    	listnode *bantu;
+    	Element *bantu;
     	bantu=head;
     	
         if (!isEmpty()){
-            for (int i=top;i>=0;i--)
-                std::cout<<stack[i].data<<std::endl;
+            for(int i=stack.top;i>=0;i--)
+                cout<<stack.data<<endl;
         } else {
-            std::cout<<"Stack Kosong"<<std::endl;
+            cout<<"Stack Kosong"<<endl;
         }
     }
     int getTop(){
-        return top;
+        return stack.top;
     }
 };
 
 int main()
 {
     MyStack s;
-    s.push(8);
-    s.push(3);
-    s.push(2);
-    s.push(9);
-    s.pop();
-    s.pop();
-    s.pop();
-    s.pop();
-    s.printStackList();
-    return 0;
+    int pil;
+	int databaru;
+	do{
+		cout<<"linked List"<<endl;
+		cout<<"-----------"<<endl<<endl;
+		cout<<"1.Push"<<endl;
+		cout<<"2.Pop"<<endl;
+		cout<<"3.Top Stack"<<endl;
+		cout<<"4.Tampil"<<endl;
+		cout<<"5.Exit"<<endl;
+		cout<<"pilihan anda= ";
+		cin>>pil;
+		switch(pil){
+			case 1:cout<<endl;
+			cout<<"data: ";
+			cin>>databaru;
+			cout<<endl;
+			void push(int databaru);break;
+
+			case 2:
+			cout<<endl;
+			Element pop();
+			break;
+
+			case 3:
+			cout<<endl;
+			int isEmpty();
+			if(isEmpty()!=0){
+			cout<<s.getTop();
+		}else{
+			cout<<"Stack kosong";
+		}break;
+			
+			case 4:
+			cout<<endl;
+			void printStackList();
+			break;
+			
+			default:cout<<"\tpilihan kosong";break;
+			
+		}
+	}
+	while (pil!=4);
 }
